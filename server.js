5b2f37a715793e0330aff6508b7591f25dd4e496
Shayan <Shayansadeghi1996@gmail.com>
2020-05-21 13:31:43 +0430

    Make connection to Database

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require('express');
-
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+//Connect to MongoDB
+mongoose
+  .connect("mongodb://localhost:27017/NoteApp")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
