2138be6c0e912d57619f7f700eb9cb161bc18cf4
Shayan <Shayansadeghi1996@gmail.com>
2020-05-21 15:30:40 +0430

    Use bcrypt module to encrypt passwords

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
 const User = require("../../models/User");
 
@@ -9,25 +10,32 @@ router.post("/register", (req, res) => {
       error: "All fields are required",
     });
   } else {
-    User.findOne({ email: req.body.email }).then((user) => {
-      if (user) {
-        res.status(400).json({
-          error: "User exists",
-        });
-      } else {
-        const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password,
-        });
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (user) {
+          res.status(400).json({
+            error: "User exists",
+          });
+        } else {
+          const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+          });
 
-        newUser
-          .save()
-          .then((user) => res.json(user))
-          .catch((err) =>console.log(err));
-      }
-    })
-    .catch(err=>res.status(400).json(err));
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) throw err;
+              newUser.password = hash;
+              newUser
+                .save()
+                .then((user) => res.json(user))
+                .catch((err) => console.log(err));
+            });
+          });
+        }
+      })
+      .catch((err) => res.status(400).json(err));
   }
 });
 
