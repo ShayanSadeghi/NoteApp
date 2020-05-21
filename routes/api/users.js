a6e137480d3947e31554c7e62b7d0f9e3f04981f
Shayan <Shayansadeghi1996@gmail.com>
2020-05-21 15:13:47 +0430

    User register

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const User = require("../../models/User");
 
+router.post("/register", (req, res) => {
+  if (!req.body.email || !req.body.password || !req.body.name) {
+    res.status(400).json({
+      error: "All fields are required",
+    });
+  } else {
+    User.findOne({ email: req.body.email }).then((user) => {
+      if (user) {
+        res.status(400).json({
+          error: "User exists",
+        });
+      } else {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+        });
 
-module.exports =router;
+        newUser
+          .save()
+          .then((user) => res.json(user))
+          .catch((err) =>console.log(err));
+      }
+    })
+    .catch(err=>res.status(400).json(err));
+  }
+});
+
+module.exports = router;
