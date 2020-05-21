cd011e71234d1c943549f20e1658c2b0da4aac9e
Shayan <Shayansadeghi1996@gmail.com>
2020-05-21 13:46:09 +0430

    Use routes in server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+
+const users = require('./routes/api/users');
+const notes = require('./routes/api/notes');
+
 const app = express();
 
 //Connect to MongoDB
@@ -8,6 +12,11 @@ mongoose
   .connect("mongodb://localhost:27017/NoteApp")
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
+
+
+//Use routes
+app.use('/api/users',users);
+app.use('/api/notes',notes);
 
 const port = process.env.PORT || 5000;
 
