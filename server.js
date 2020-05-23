b42703d9352fcd38ba92414c607bbae25c2c544e
Shayan <Shayansadeghi1996@gmail.com>
2020-05-23 14:20:56 +0430

    added passport middleware and config it

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const notes = require('./routes/api/notes');
@@ -17,6 +18,8 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 //Use routes
 app.use('/api/users',users);
