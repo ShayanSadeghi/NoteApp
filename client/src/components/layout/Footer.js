de42a7e5890ad3075e7f8aa57235288401f36d5a
Shayan <Shayansadeghi1996@gmail.com>
2020-05-27 14:22:50 +0430

    fixed a typo

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default () => {
   return (
-    <footer clasName="text-white bg-dark mt-5 p-4 text-center">
+    <footer className="text-white bg-dark mt-5 p-4 text-center">
       Copyright &copy; {new Date().getFullYear()} NoteApp
     </footer>
   );
