e8cedf431c744e134dd8ff6d6278f83399d10385
Shayan <Shayansadeghi1996@gmail.com>
2020-05-27 11:47:45 +0430

    added Navbar

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            NoteApp
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="mobile-nav">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Notes
+                </a>
+              </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="register.html">
+                  Sign-up
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">
+                  About
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
