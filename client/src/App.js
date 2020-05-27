150ee2d1fbd941f72e991206d61e2f65a6b85c79
Shayan <Shayansadeghi1996@gmail.com>
2020-05-27 12:09:59 +0430

    added Signup component

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Signup from "./components/layout/Signup";
 
 import "./App.css";
-
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-
-
+      <Signup />
       <Footer />
     </div>
   );
---
+++ client/src/components/layout/Signup.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+
+class Signup extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay text-light landing-inner">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-2 mb-4">Note App</h1>
+                <p className="lead">
+                  Write your Notes once, Keep them with your self any where
+                </p>
+                <hr />
+              </div>
+              <div className="container col-md-6  text-light text-center">
+                <div className="sign-up-form ">
+                  <form>
+                    <div className="form-group ">
+                      <label for="name">Name</label>
+                      <input className="form-control" type="text" id="name" />
+                    </div>
+                    <div className="form-group">
+                      <label for="email">Email</label>
+                      <input className="form-control" type="email" id="email" />
+                    </div>
+                    <div className="form-group">
+                      <label for="password">Password</label>
+                      <input
+                        className="form-control"
+                        type="password"
+                        id="password"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label for="password2">Confirm password</label>
+                      <input
+                        className="form-control"
+                        type="password"
+                        id="password2"
+                      />
+                    </div>
+                    <button type="submit" className="btn btn-primary mr-5">
+                      Sign Up
+                    </button>
+                    <a href="login.html" className="text-secondary">
+                      Login
+                    </a>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Signup;
