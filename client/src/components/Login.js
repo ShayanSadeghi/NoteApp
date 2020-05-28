606ce780f9e3a7933cb58833405e798e6c48b452
Shayan <Shayansadeghi1996@gmail.com>
2020-05-28 23:06:12 +0430

    added value and name props, and onChange handler

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,33 @@
 import React, { Component } from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors:{}
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+  }
+
+  onChange(e){
+    this.setState({[e.target.name]: e.target.value});
+  }
+  
+  onSubmit(e){
+    e.preventDefault();
+
+    const userData = {
+      email : this.state.email,
+      password: this.state.password
+    }
+    console.log(userData);
+  }
+
   render() {
     return (
       <div className="landing">
@@ -17,14 +43,24 @@ class Login extends Component {
               </div>
               <div className="container col-md-6 text-light text-center">
                 <div className="sign-up-form">
-                  <form>
+                  <form noValidate onSubmit={this.onSubmit}>
                     <div className="form-group">
                       <label htmlFor="email">Email</label>
-                      <input className="form-control" type="email" id="email" />
+                      <input
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.onChange}
+                        className="form-control"
+                        type="email"
+                        id="email"
+                      />
                     </div>
                     <div className="form-group">
                       <label htmlFor="password">Password</label>
                       <input
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.onChange}
                         className="form-control"
                         type="password"
                         id="password"
