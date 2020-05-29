db8c9ed4a6c11872ebf79081e6b043de683c3955
Shayan <Shayansadeghi1996@gmail.com>
2020-05-29 13:27:59 +0430

    initiate redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Route exact path="/" component={Signup} />
-        <Route exact path="/login" component={Login} />
-      </Router>
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <Navbar />
+          <Route exact path="/" component={Signup} />
+          <Route exact path="/login" component={Login} />
+        </Router>
+        <Footer />
+      </Provider>
+    );
+  }
 }
 
 export default App;
