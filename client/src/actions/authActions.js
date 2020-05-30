37260fcbf85b907faa0a17ae629ebc67f172550b
Shayan <Shayansadeghi1996@gmail.com>
2020-05-30 13:15:01 +0430

    added login action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,13 +1,39 @@
 import axios from "axios";
-import jwt_decode from 'jwt-decode';
+import jwt_decode from "jwt-decode";
+import setAuthToken from "../utils/setAuthToken";
 
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 //Register User
 export const registerUser = (userData, history) => dispatch => {
   axios
     .post("/api/users/register", userData)
     .then(() => history.push("/login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+//Login User
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      const { token } = res.data;
+      // Set token to local-storage
+      localStorage.setItem("jwtToken", token);
+      // set token to axios auth header
+      setAuthToken(token);
+      // Decode token
+      const decodedUser = jwt_decode(token);
+      dispatch({
+        type: SET_CURRENT_USER,
+        payload: decodedUser,
+      });
+    })
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
