d724710732e66e331387cbf057649a89d09f406d
Shayan <Shayansadeghi1996@gmail.com>
2020-05-31 17:12:18 +0430

    Show diffrent nav buttons for guest and users

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,8 +1,77 @@
 import React, { Component } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
 
 class Navbar extends Component {
+  onLogoutClick = e => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
+
   render() {
+    const { isAuthenticated, user } = this.props.auth;
+    let NavContent;
+
+    if (!isAuthenticated) {
+      //Guest Content
+      NavContent = (
+        <div className="container">
+          <ul className="navbar-nav ml-auto">
+            <li className={classnames("nav-item")}>
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
+            <li className={classnames("nav-item")}>
+              <Link className="nav-link" to="/">
+                Sign-Up
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/notes">
+                Docs
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+      );
+    } else {
+      //User Content
+      NavContent = (
+        <div className="container">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/notes">
+                Notes
+              </Link>
+            </li>
+          </ul>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                to=""
+                onClick={this.onLogoutClick.bind(this)}>
+                Logout
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                {user.name}
+              </Link>
+            </li>
+          </ul>
+        </div>
+      );
+    }
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
@@ -17,25 +86,7 @@ class Navbar extends Component {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="mobile-nav">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/notes">
-                  Notes
-                </Link>
-              </li>
-            </ul>
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Sign-up
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">
-                  About
-                </Link>
-              </li>
-            </ul>
+            {NavContent}
           </div>
         </div>
       </nav>
@@ -43,4 +94,13 @@ class Navbar extends Component {
   }
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  auth: PropTypes.object.isRequired,
+  logoutUser: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logoutUser })(Navbar);
