b6ac7277fd3ed11f80a40db6c4f03e1e3dfc9ffd
Shayan <Shayansadeghi1996@gmail.com>
2020-06-01 16:36:13 +0430

    Added user auth by using local storage and check for it's expiration

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,32 @@ import Footer from "./components/layout/Footer";
 import Signup from "./components/auth/Signup";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearNotes } from "./actions/notesActions";
+import jwt_decode from "jwt-decode";
 
 import "./App.css";
+
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Clear current Profile
+    store.dispatch(clearNotes());
+    // Redirect to login
+    window.location.href = "/login";
+  }
+}
 
 class App extends Component {
   render() {
