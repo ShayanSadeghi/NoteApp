82f9861bceaa95ef568e8e77e40c560b514ed2ea
Shayan <Shayansadeghi1996@gmail.com>
2020-06-10 22:00:43 +0430

    initiate PWA including using service workerto cache contents, add some stuff to the  manifest file

---
+++ client/public/worker.js
@@ -0,0 +1,85 @@
+let CACHE_STATIC_NAME = "static";
+let CACHE_DYNAMIC_NAME = "dynamic";
+
+const STATIC_FILES = ["/"];
+
+self.addEventListener("install", function (event) {
+  console.log("[Service Worker] Installing Service Worker ...", event);
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME).then(function (cache) {
+      console.log("[Service Worker] Precaching App Shell");
+      cache.addAll(STATIC_FILES);
+    })
+  );
+});
+
+self.addEventListener("activate", e => {
+  console.log("[Service Worker] Activating Service Worker ...", e);
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      //Use Promise.all for feuture if we have multiple caches and want to run them concurrently
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log("[Service Worker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+function isInArray(string, array) {
+  var cachePath;
+  if (string.indexOf(self.origin) === 0) {
+    // request targets domain where we serve the page from (i.e. NOT a CDN)
+    console.log("matched ", string);
+    cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
+  } else {
+    cachePath = string; // store the full request (for CDNs)
+  }
+  return array.indexOf(cachePath) > -1;
+}
+
+self.addEventListener("fetch", function (event) {
+  var url = "https://httpbin.org/get";
+  if (event.request.url.indexOf(url) > -1) {
+    event.respondWith(
+      caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
+        return fetch(event.request).then(function (res) {
+          // trimCache(CACHE_DYNAMIC_NAME, 3);
+          cache.put(event.request, res.clone());
+          return res;
+        });
+      })
+    );
+  } else if (isInArray(event.request.url, STATIC_FILES)) {
+    event.respondWith(caches.match(event.request));
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(function (response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then(function (res) {
+              return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
+                // trimCache(CACHE_DYNAMIC_NAME, 3);
+                cache.put(event.request.url, res.clone());
+                return res;
+              });
+            })
+            .catch(function (err) {
+              return caches.open(CACHE_STATIC_NAME).then(function (cache) {
+                if (event.request.headers.get("accept").includes("text/html")) {
+                  return cache.match("/offline.html");
+                }
+              });
+            });
+        }
+      })
+    );
+  }
+});
--- client/src/index.js
+++ client/src/index.js
@@ -10,4 +10,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-serviceWorker.unregister();
+serviceWorker.register();
