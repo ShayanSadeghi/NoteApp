b8437bbd7815d00332e7b51a358859b3b94f7237
Shayan <Shayansadeghi1996@gmail.com>
2020-06-21 22:16:28 +0430

    fix problem with long notes

--- client/src/components/dashboard/ShowCards.js
+++ client/src/components/dashboard/ShowCards.js
@@ -5,8 +5,17 @@ export default function ShowCards({ note, editHandler, deleteHandler }) {
     <div className="w18rem card  mr-auto mb-3 ">
       <div className="card-body">
         <img src="" alt="" className="card-img-top" />
-        <h5 className="card-title">{note.title}</h5>
-        <p className="card-text">{note.body}</p>
+        <h5 className="card-title">
+          {" "}
+          {note.title.length < 20
+            ? note.title
+            : note.title.slice(0, 20) + " ..."}
+        </h5>
+        <p className="card-text ">
+          {note.body.length < 100
+            ? note.body
+            : note.body.slice(0, 100) + " ..."}
+        </p>
         <button
           onClick={() => editHandler(note._id)}
           className="btn btn-outline-primary mr-2">
