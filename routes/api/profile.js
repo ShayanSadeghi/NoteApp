ae1c2556837f2151b8c25cb417e1c9a45d277721
Shayan <Shayansadeghi1996@gmail.com>
2020-06-23 19:49:54 +0430

    added /upload route for user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,56 @@
+const path = require("path");
+const crypto = require("crypto");
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
 const passport = require("passport");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const methodOverride = require("method-override");
 
 const Profile = require("../../models/Profile");
+
+//Mongo
+const mongoURI = "mongodb://localhost:27017/NoteApp";
+const conn = mongoose.createConnection(mongoURI, () => {
+  console.log("MongoDB Connected");
+});
+
+//Init gfs
+let gfs;
+
+conn.once("open", () => {
+  //Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("uploads");
+});
+
+//Create storage engine
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: "uploads",
+        };
+        resolve(fileInfo);
+      });
+    });
+  },
+});
+
+const upload = multer({ storage });
+
+router.post("/upload", upload.single("inputFile"), (req, res) => {
+  res.json({ file: req.file });
+});
 
 router.get(
   "/",
