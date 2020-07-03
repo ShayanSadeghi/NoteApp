c0c409de2fa19af4d57e4e58010511cf78fcaf38
Shayan <Shayansadeghi1996@gmail.com>
2020-07-03 22:02:23 +0430

    added simple chart

--- client/src/components/stats/Stats.js
+++ client/src/components/stats/Stats.js
@@ -1,15 +1,61 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { Bar, Line } from "react-chartjs-2";
 import { getUserNotes } from "../../actions/notesActions";
 
 class Stats extends Component {
+  constructor() {
+    super();
+    this.state = {
+      chartData: {
+        labels: [],
+        datasets: [
+          {
+            label: "Notes",
+            data: [],
+          },
+        ],
+      },
+    };
+  }
+
   componentDidMount() {
     this.props.getUserNotes();
   }
+  componentWillReceiveProps(nextProp) {
+    if (nextProp.notes) {
+      let newLabels = [];
+      let newData = [];
+      nextProp.notes.forEach(note => {
+        const date = note.date.slice(0, 10);
+        if (newLabels.indexOf(date) === -1) {
+          newLabels.push(date);
+          const newNotes = nextProp.notes.filter(
+            note => note.date.slice(0, 10) === date
+          );
+          newData.push(newNotes.length);
+        }
+      });
+      const chartData = {
+        labels: newLabels,
+        datasets: [
+          {
+            label: "Notes",
+            data: newData,
+          },
+        ],
+      };
+      this.setState({ chartData });
+    }
+  }
 
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Bar data={this.state.chartData} />
+      </div>
+    );
   }
 }
 
